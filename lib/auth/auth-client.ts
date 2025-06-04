969729fa663b29f006de259e89ccf766f7930bb6
Andrei Hudovich <andrei.hudovich@gmail.com>
2025-06-04 22:04:55 +0200

    fix: fix issue with base url

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -1,7 +1,8 @@
 import { emailOTPClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
+import { BASE_URL } from '@/enums/constants'
 
 export const authClient = createAuthClient({
   plugins: [emailOTPClient()],
-  baseURL: process.env.NEXT_PUBLIC_WEBSITE_BASE_URL,
+  baseURL: BASE_URL,
 })
--- lib/constants.ts
+++
@@ -1,6 +0,0 @@
-import type { Env } from '@/types/env'
-
-export const BASE_URL =
-  (process.env.NEXT_PUBLIC_ENV as Env) !== 'preview'
-    ? process.env.NEXT_PUBLIC_WEBSITE_BASE_URL
-    : `https://${process.env.VERCEL_BRANCH_URL}`
