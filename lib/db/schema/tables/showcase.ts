2cb5cb08cb4a0e9acb987951446ccc90465e9335
Andrei Hudovich <andrei.hudovich@gmail.com>
2025-06-09 20:31:27 +0200

    feat: add pricing url column

--- lib/db/schema/tables/showcase.ts
+++ lib/db/schema/tables/showcase.ts
@@ -1,6 +1,6 @@
 import { boolean, integer, pgTable, text } from 'drizzle-orm/pg-core'
-import { domainRatings, id, linkAttributes, timestamps } from '../helpers/columns'
-import { pricingModelEnum, productCategoryEnum, TABLE_NAMES } from '../helpers/enums'
+import { domainRatings, id, linkAttributes, pricing, timestamps } from '../helpers/columns'
+import { productCategoryEnum, TABLE_NAMES } from '../helpers/enums'
 
 export const showcases = pgTable(TABLE_NAMES.SHOWCASE, {
   ...id,
@@ -12,8 +12,7 @@ export const showcases = pgTable(TABLE_NAMES.SHOWCASE, {
   ...domainRatings,
   traffic: integer().notNull(),
   webAnalyticsUrl: text().unique(),
-  pricingModel: pricingModelEnum().notNull(),
-  pricingInfo: text(),
+  ...pricing,
   ...linkAttributes,
   isAccountRequired: boolean().notNull(),
   submitUrl: text().unique(),
