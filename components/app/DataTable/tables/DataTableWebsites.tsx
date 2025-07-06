fab22135adbc82d0c697232fe8952473400036eb
Andrei Hudovich <andrei.hudovich@gmail.com>
2025-07-06 19:12:38 +0200

    feat(app/filters): add search filter

--- components/app/DataTable/tables/DataTableWebsites.tsx
+++ components/app/DataTable/tables/DataTableWebsites.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { flexRender } from '@tanstack/react-table'
+import { DataTableFilters } from '@/components/app/DataTable/DataTableFilters/DataTableFilters'
 import DataTableHeaderCell from '@/components/app/DataTable/DataTableHeaderCell'
-import { useDataTable } from '@/hooks/useDataTable'
+import { useWebsiteDataTable } from '@/hooks/useWebsiteDataTable'
 import type { AccessorKeyColumnDef } from '@tanstack/react-table'
 
 interface DataTableWebsitesProps<T> {
@@ -11,48 +12,52 @@ interface DataTableWebsitesProps<T> {
 }
 
 export default function DataTableWebsites<T>({ data, columns }: DataTableWebsitesProps<T>) {
-  const { table } = useDataTable<T>({
+  const { table } = useWebsiteDataTable<T>({
     initialSorting: [{ id: 'dr', desc: true }],
     columns,
     data,
   })
 
   return (
-    <table className="w-full">
-      <thead className="sticky top-0 z-[1] bg-white">
-        {table.getHeaderGroups().map(({ id, headers }) => (
-          <tr key={id}>
-            {headers.map((header) => (
-              <DataTableHeaderCell
-                key={header.id}
-                isSortable={header.column.getCanSort()}
-                sortingDirection={header.column.getIsSorted()}
-                size={header.column.columnDef.size}
-                // @ts-expect-error - TODO: fix this
-                tooltip={header.column.columnDef.meta?.tooltip}
-                onClick={header.column.getToggleSortingHandler()}
-              >
-                {flexRender(header.column.columnDef.header, header.getContext())}
-              </DataTableHeaderCell>
-            ))}
-          </tr>
-        ))}
-      </thead>
+    <>
+      <DataTableFilters className="p-4 border-b border-b-layout-separator" table={table} />
 
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr className="group transition-colors hover:bg-zinc-100" key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td
-                className="px-4 py-3 border-y border-y-layout-separator font-medium text-xs group-first:border-t-0"
-                key={cell.id}
-              >
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+      <table className="w-full">
+        <thead className="sticky top-0 z-[1] bg-white">
+          {table.getHeaderGroups().map(({ id, headers }) => (
+            <tr key={id}>
+              {headers.map((header) => (
+                <DataTableHeaderCell
+                  key={header.id}
+                  isSortable={header.column.getCanSort()}
+                  sortingDirection={header.column.getIsSorted()}
+                  size={header.column.columnDef.size}
+                  // @ts-expect-error - TODO: fix this
+                  tooltip={header.column.columnDef.meta?.tooltip}
+                  onClick={header.column.getToggleSortingHandler()}
+                >
+                  {flexRender(header.column.columnDef.header, header.getContext())}
+                </DataTableHeaderCell>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr className="group transition-colors hover:bg-zinc-100" key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  className="px-4 py-3 border-y border-y-layout-separator font-medium text-xs group-first:border-t-0"
+                  key={cell.id}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
--- components/ui/BaseInput.tsx
+++ components/ui/BaseInput.tsx
@@ -1,16 +1,29 @@
 import { twMerge } from 'tailwind-merge'
 import { tv } from 'tailwind-variants'
+import BaseIcon from './BaseIcon'
 import type { ChangeEvent, ComponentProps } from 'react'
+import type { IconSvgElement } from '@hugeicons/react'
 import type { VariantProps } from 'tailwind-variants'
 
 const inputVariants = tv({
   slots: {
     base: '',
     label: 'inline-block mb-1.5 text-sm font-medium text-secondary cursor-pointer',
+    icon: 'absolute top-1/2 -translate-y-1/2 size-4',
     input:
       'block px-3 w-full bg-control-default border border-control-default rounded-control outline-none font-medium text-control-default appearance-none transition-colors placeholder:text-control-placeholder focus:bg-control-active focus:border-control-active focus:ring-2 focus:ring-control-default focus:text-control-active',
   },
   variants: {
+    iconPosition: {
+      left: {
+        icon: 'left-3',
+        input: 'pl-9',
+      },
+      right: {
+        icon: 'right-3',
+        input: 'pr-9',
+      },
+    },
     size: {
       xs: {
         label: 'text-xs',
@@ -31,19 +44,23 @@ const inputVariants = tv({
     },
   },
   defaultVariants: {
+    iconPosition: 'left',
     size: 'md',
   },
 })
 
 type InputVariants = VariantProps<typeof inputVariants>
+type NativeInputProps = Omit<ComponentProps<'input'>, 'size'>
 
 interface BaseInputProps {
   id: string
-  value?: string
   className?: string
+  icon?: IconSvgElement
+  iconPosition?: InputVariants['iconPosition']
   rootClasses?: string
   label?: string
   size?: InputVariants['size']
+  value?: string
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
@@ -52,12 +69,19 @@ export default function BaseInput({
   id,
   label,
   rootClasses,
+  icon,
+  iconPosition,
   size,
   value,
   onChange,
   ...props
-}: BaseInputProps & ComponentProps<'input'>) {
-  const { base, label: labelVariants, input } = inputVariants({ size })
+}: BaseInputProps & NativeInputProps) {
+  const {
+    base,
+    label: labelVariants,
+    input,
+    icon: iconVariants,
+  } = inputVariants({ iconPosition, size })
 
   return (
     <div className={twMerge(base(), rootClasses)}>
@@ -67,13 +91,17 @@ export default function BaseInput({
         </label>
       )}
 
-      <input
-        id={id}
-        className={input({ className })}
-        value={value}
-        onChange={onChange}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          id={id}
+          className={input({ className })}
+          value={value}
+          onChange={onChange}
+          {...props}
+        />
+
+        {icon && <BaseIcon className={iconVariants()} icon={icon} strokeWidth={2.5} />}
+      </div>
     </div>
   )
 }
