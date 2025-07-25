c6fd88a4230c9dfafd53c8e32c807015df6606fc
Andrei Hudovich <andrei.hudovich@gmail.com>
2025-07-25 16:32:58 +0200

    feat(app): update link attribute and account columns

--- components/app/DataTable/DataTableCellAccount.tsx
+++ components/app/DataTable/DataTableCellAccount.tsx
@@ -1,15 +1,23 @@
-import { Cancel01Icon, Tick02Icon } from '@hugeicons/core-free-icons'
+import { Cancel01Icon, CheckmarkCircle02Icon } from '@hugeicons/core-free-icons'
 import BaseIcon from '@/components/ui/BaseIcon'
+import { cn } from '@/utils/css'
 
 export default function DataTableCellAccount({ value }: { value: boolean }) {
   return (
-    <div className="flex items-center gap-1 capitalize">
+    <div
+      className={cn(
+        'inline-flex items-center justify-center gap-1 px-2 py-0.5 min-w-16 rounded-full font-semibold text-[0.625rem] leading-[0.875rem] tracking-[0.01rem] whitespace-nowrap uppercase',
+        value
+          ? 'text-slate-800 bg-slate-100 border border-slate-600/50'
+          : 'text-slate-800 bg-slate-100/25 border border-slate-600/25'
+      )}
+    >
       <BaseIcon
-        className="shrink-0 size-4 text-secondary"
-        icon={value ? Tick02Icon : Cancel01Icon}
+        className={cn('shrink-0 size-3', value ? 'text-slate-700' : 'text-slate-700')}
+        icon={value ? CheckmarkCircle02Icon : Cancel01Icon}
         strokeWidth={2.5}
       />
-      {value ? 'Yes' : 'No'}
+      {value ? 'Required' : 'Optional'}
     </div>
   )
 }
