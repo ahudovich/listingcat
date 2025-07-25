0a64c0a9a49b7b62dbb0653ec10a0ced7c13aa29
Andrei Hudovich <andrei.hudovich@gmail.com>
2025-07-25 14:07:11 +0200

    feat(app): show n/a for traffic column

--- components/app/DataTable/DataTableCellTraffic.tsx
+++ components/app/DataTable/DataTableCellTraffic.tsx
@@ -1,5 +1,7 @@
 import { formatNumber } from '@/utils/formatters'
 
 export function DataTableCellTraffic({ value }: { value: number }) {
-  return <div className="flex items-center gap-x-1.5">{formatNumber(value)}</div>
+  return (
+    <div className="flex items-center gap-x-1.5">{value === 0 ? 'N/A' : formatNumber(value)}</div>
+  )
 }
