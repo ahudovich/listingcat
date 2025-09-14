593f2e729a02c8634f9f8f0dd21f318664f652cf
Andrei Hudovich <andrei@hudovich.com>
2025-09-14 21:27:18 +0200

    feat: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Footer from '@/components/layout/Footer/Footer'
+import Header from '@/components/layout/Header'
+import { BaseButton } from '@/components/ui/BaseButton'
+
+export default function NotFound() {
+  return (
+    <>
+      <Header />
+      <main className="container-article py-12 md:pb-20">
+        <h1 className="mb-2 text-primary text-2xl font-bold sm:text-3xl">Page Not Found</h1>
+        <p className="mb-6">The page you are looking for does not exist.</p>
+        <BaseButton asChild>
+          <Link href="/">Return to home</Link>
+        </BaseButton>
+      </main>
+      <Footer />
+    </>
+  )
+}
