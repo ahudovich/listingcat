0098018520f5b0fe205fc5e52b3e83eb27332ea1
Andrei Hudovich <andrei@hudovich.com>
2025-10-02 13:59:52 +0200

    refactor(app): remove SidebarWrapper component and simplify layout structure

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,5 +1,4 @@
 import { AppHeader } from '@/components/app/layout/AppHeader/AppHeader'
-import { SidebarWrapper } from '@/components/app/layout/SidebarWrapper'
 import { Sidebar } from '@/components/app/Sidebar/Sidebar'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
@@ -7,9 +6,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <div className="grid grid-rows-[auto_1fr] h-dvh">
       <AppHeader />
       <main className="overflow-hidden lg:grid lg:grid-cols-[auto_1fr] lg:h-full">
-        <SidebarWrapper>
-          <Sidebar />
-        </SidebarWrapper>
+        <Sidebar />
 
         <div className="overflow-hidden bg-white lg:grid lg:grid-rows-[auto_1fr] lg:h-full">
           {children}
