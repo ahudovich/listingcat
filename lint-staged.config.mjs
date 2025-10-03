d00514818d29ca4835c95da22ad8dcde3f38a24a
Andrei Hudovich <andrei@hudovich.com>
2025-10-03 12:11:25 +0200

    chore: fix lint-staged setup issue

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,11 +1,11 @@
 import path from 'node:path'
 
-const buildEslintCommand = (filenames) =>
+const eslintCommand = (filenames) =>
   `doppler run -p lc-main -c dev -- next lint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
 
 const config = {
-  '*.{ts,tsx}': [() => 'tsc -p tsconfig.json --noEmit', buildEslintCommand],
-  '*.{js,mjs,cjs,ts,tsx,css}': ['prettier --check .'],
+  '*.{ts,tsx}': [() => 'tsc -p tsconfig.json --noEmit', eslintCommand],
+  '*.{js,mjs,cjs,ts,tsx,css}': ['prettier --check'],
 }
 
 export default config
